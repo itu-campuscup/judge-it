2353f5d01568d69fa1817dbe316c568f11b4a50c
Nadja Brix Koch <nadjabrixkoch@proton.me>
2026-03-01 20:44:42 +0100

    refactor(judges): move heat logic to hook for remaining judges
    Again, the code was basically copy pasted into the hook without
    modification except for some renaming of variables. I had to do some
    changes to the getter for the previous player, just to ensure that it
    returned a result that was useful with the refactorisation.
    
    Signed-off-by: Nadja Brix Koch <nadjabrixkoch@proton.me>

--- src/app/hooks/useHeatControls.ts
+++ src/app/hooks/useHeatControls.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useMutation } from "convex/react";
 import { api } from "convex/_generated/api";
 import { Id } from "convex/_generated/dataModel";
-import { getCurrentHeat } from "@/utils/getUtils";
+import { getCurrentHeat, getTimeType } from "@/utils/getUtils";
 import { TIME_TYPE_SAIL } from "@/utils/constants";
 import type { AlertObject, Heat } from "@/types";
 import useFetchDataConvex from "../hooks/useFetchDataConvex";
@@ -22,6 +22,7 @@ const useHeatControls = (
   const createHeat = useMutation(api.mutations.createHeat);
   const setCurrentHeat = useMutation(api.mutations.setCurrentHeat);
   const createTimeLogsBatch = useMutation(api.mutations.createTimeLogsBatch);
+  const createTimeLog = useMutation(api.mutations.createTimeLog);
   const [nextHeatNumber, setNextHeatNumber] = useState<number>(1);
 
   // Calculate next heat number when component mounts
@@ -167,6 +168,164 @@ const useHeatControls = (
   };
 
   /**
+   * Handle button click to start/stop the timers to send a row to the db
+   */
+  const insertTimeLog = async (
+    timeTypeId: Id<"time_types">,
+    player?: Id<"players"> | null,
+    team?: Id<"teams">,
+  ): Promise<void> => {
+    const currentHeat = getCurrentHeat(heats, alert);
+    if (!currentHeat) {
+      alert.setOpen(true);
+      alert.setSeverity("error");
+      alert.setText("No current heat found");
+      alert.setContext({
+        operation: "record_time",
+        location: "BeerJudge.insertTimeLog",
+        metadata: {
+          timeTypeId,
+          team,
+          player,
+        },
+      });
+      return;
+    }
+
+    const isValid = validateInputs("insertTimeLog", timeTypeId, player, team);
+    if (!isValid) return;
+
+    try {
+      await createTimeLog({
+        team_id: team as Id<"teams">,
+        player_id: player as Id<"players">,
+        time_type_id: timeTypeId,
+        heat_id: currentHeat.id,
+      });
+    } catch (error) {
+      const err = "Error inserting time log: " + (error as Error).message;
+      alert.setOpen(true);
+      alert.setSeverity("error");
+      alert.setText(err);
+      alert.setContext({
+        operation: "record_time",
+        location: "BeerJudge.insertTimeLog",
+        metadata: {
+          step: "insert_time_log",
+          teamId: team,
+          playerId: player,
+          timeTypeId,
+          heatId: currentHeat.id,
+        },
+      });
+      return;
+    }
+    const timeTypeName =
+      timeTypes.find((e) => e.id === timeTypeId)?.time_eng || "Unknown";
+    alert.setOpen(true);
+    alert.setSeverity("success");
+    alert.setText(`Inserted log of type: ${timeTypeName}`);
+    alert.setContext({
+      operation: "record_time",
+      location: "BeerJudge.insertTimeLog",
+      metadata: {
+        teamId: team,
+        playerId: player,
+        timeTypeId,
+        timeTypeName,
+        heatId: currentHeat.id,
+      },
+    });
+  };
+
+  const handleStartStop = async (
+    prevPlayerId: Id<"players"> | null,
+    playerId?: Id<"players"> | null,
+    teamId?: Id<"teams">,
+  ) => {
+    const currentHeat = getCurrentHeat(heats, alert);
+    if (!validateInputs("handleStartStop", playerId, teamId, currentHeat)) {
+      alert.setOpen(true);
+      alert.setSeverity("error");
+      alert.setText("Missing parameters");
+      alert.setContext({
+        operation: "start_stop_timer",
+        location: "useHeatControls.handleStartStop",
+      });
+      return;
+    }
+
+    const timeTypeId = getTimeType(TIME_TYPE_SAIL, timeTypes)?.id;
+    if (!timeTypeId) {
+      alert.setOpen(true);
+      alert.setSeverity("error");
+      alert.setText("Could not find sailing time type");
+      alert.setContext({
+        operation: "start_stop_timer",
+        location: "useHeatControls.handleStartStop",
+        metadata: {
+          searchingFor: TIME_TYPE_SAIL,
+          availableTimeTypes: timeTypes.map((t) => t.time_eng),
+        },
+      });
+      return;
+    }
+
+    try {
+      await createTimeLogsBatch({
+        logs: [
+          {
+            team_id: teamId,
+            player_id: prevPlayerId!,
+            time_type_id: timeTypeId,
+            heat_id: currentHeat!.id,
+          },
+          {
+            team_id: teamId,
+            player_id: playerId!,
+            time_type_id: timeTypeId,
+            heat_id: currentHeat!.id,
+          },
+        ],
+      });
+    } catch (error) {
+      const err = "Error inserting time log: " + (error as Error).message;
+      alert.setOpen(true);
+      alert.setSeverity("error");
+      alert.setText(err);
+      alert.setContext({
+        operation: "start_stop_timer",
+        location: "useHeatControls.handleStartStop",
+        metadata: {
+          step: "insert_time_logs",
+          teamId: teamId,
+          prevPlayerId,
+          currentPlayerId: playerId,
+          heatId: currentHeat!.id,
+        },
+      });
+      return;
+    }
+    alert.setOpen(true);
+    alert.setSeverity("success");
+    alert.setText(
+      "Inserted log of type: " +
+        (timeTypes.find((e) => e.id === timeTypeId)?.time_eng || "Unknown"),
+    );
+    alert.setContext({
+      operation: "start_stop_timer",
+      location: "useHeatControls.handleStartStop",
+      metadata: {
+        teamId: teamId,
+        prevPlayerId,
+        currentPlayerId: playerId,
+        heatId: currentHeat!.id,
+        timeType: TIME_TYPE_SAIL,
+      },
+    });
+  };
+
+  /**
    * Check all inputs are present
    * Otherwise create alert
    */
@@ -189,7 +348,7 @@ const useHeatControls = (
     return true;
   };
 
-  return { nextHeatNumber, handleGlobalStart };
+  return { nextHeatNumber, handleGlobalStart, insertTimeLog, handleStartStop };
 };
 
 export default useHeatControls;
