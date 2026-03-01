6cac22d51f0601b62725a6e3f2a25747e972220a
Nadja Brix Koch <nadjabrixkoch@proton.me>
2026-03-01 20:44:42 +0100

    refactor(judges): move heat logic to hook for remaining judges
    Again, the code was basically copy pasted into the hook without
    modification except for some renaming of variables. I had to do some
    changes to the getter for the previous player, just to ensure that it
    returned a result that was useful with the refactorisation.
    
    Signed-off-by: Nadja Brix Koch <nadjabrixkoch@proton.me>

--- src/app/judge-it/BeerJudge.tsx
+++ src/app/judge-it/BeerJudge.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useMutation } from "convex/react";
-import { api } from "convex/_generated/api";
 import AlertComponent from "../components/AlertComponent";
 import { Stack } from "@mui/material";
 import {
   getPlayerName,
-  getCurrentHeat,
   getPlayerIdGivenTeamAndTimeLogs,
 } from "@/utils/getUtils";
 import {
@@ -14,17 +11,17 @@ import {
   TIME_TYPE_SPIN,
 } from "@/utils/constants";
 import type { Team, TimeType } from "@/types";
-import { Id } from "convex/_generated/dataModel";
 import useFetchDataConvex from "../hooks/useFetchDataConvex";
 import JudgeButton from "../components/JudgeButton";
+import { useHeatControls } from "../hooks";
+import { Id } from "convex/_generated/dataModel";
 
 interface BeerJudgeProps {
   selectedTeam: Team | null;
 }
 
 const BeerJudge: React.FC<BeerJudgeProps> = ({ selectedTeam }) => {
-  const { alert, heats, players, timeLogs, timeTypes } = useFetchDataConvex();
-  const createTimeLog = useMutation(api.mutations.createTimeLog);
+  const { alert, players, timeLogs, timeTypes } = useFetchDataConvex();
 
   const latestPlayer =
     selectedTeam !== null
@@ -33,6 +30,8 @@ const BeerJudge: React.FC<BeerJudgeProps> = ({ selectedTeam }) => {
   const playerName = latestPlayer
     ? getPlayerName(latestPlayer, players)
     : "player null";
+
+  const { insertTimeLog } = useHeatControls({}, alert);
 
   const timeTypeEmoji = (timeType: TimeType) => {
     switch (timeType.time_eng) {
@@ -47,106 +46,8 @@ const BeerJudge: React.FC<BeerJudgeProps> = ({ selectedTeam }) => {
     }
   };
 
-  /**
-   * Handle button click to start/stop the timers to send a row to the db
-   */
-  const handleTimeTypeClick = async (
-    timeTypeId: Id<"time_types">,
-  ): Promise<void> => {
-    const currentHeat = getCurrentHeat(heats, alert);
-    if (!currentHeat) {
-      alert.setOpen(true);
-      alert.setSeverity("error");
-      alert.setText("No current heat found");
-      alert.setContext({
-        operation: "record_time",
-        location: "BeerJudge.handleTimeTypeClick",
-        metadata: {
-          timeTypeId,
-          selectedTeam,
-          latestPlayer,
-        },
-      });
-      return;
-    }
-
-    const isValid = validateInputs();
-    if (!isValid) return;
-
-    try {
-      await createTimeLog({
-        team_id: selectedTeam?.id,
-        player_id: latestPlayer as Id<"players">,
-        time_type_id: timeTypeId,
-        heat_id: currentHeat.id,
-      });
-    } catch (error) {
-      const err = "Error inserting time log: " + (error as Error).message;
-      alert.setOpen(true);
-      alert.setSeverity("error");
-      alert.setText(err);
-      alert.setContext({
-        operation: "record_time",
-        location: "BeerJudge.handleTimeTypeClick",
-        metadata: {
-          step: "insert_time_log",
-          teamId: selectedTeam,
-          playerId: latestPlayer,
-          timeTypeId,
-          heatId: currentHeat.id,
-        },
-      });
-      return;
-    }
-    const timeTypeName =
-      timeTypes.find((e) => e.id === timeTypeId)?.time_eng || "Unknown";
-    alert.setOpen(true);
-    alert.setSeverity("success");
-    alert.setText(`Inserted log of type: ${timeTypeName}`);
-    alert.setContext({
-      operation: "record_time",
-      location: "BeerJudge.handleTimeTypeClick",
-      metadata: {
-        teamId: selectedTeam,
-        playerId: latestPlayer,
-        timeTypeId,
-        timeTypeName,
-        heatId: currentHeat.id,
-      },
-    });
-  };
-  /**
-   * Validate inputs before sending to the database
-   * @returns {boolean} True if inputs are valid, false otherwise
-   */
-  const validateInputs = (): boolean => {
-    let errorTxt: string | null = null;
-
-    if (!selectedTeam) {
-      errorTxt = "Team has not been selected";
-    } else if (!latestPlayer) {
-      errorTxt = "Player has not been selected";
-    } else if (!timeTypes.length) {
-      errorTxt = "No time types available";
-    }
-
-    if (errorTxt) {
-      alert.setOpen(true);
-      alert.setSeverity("error");
-      alert.setText(errorTxt);
-      alert.setContext({
-        operation: "validate_inputs",
-        location: "BeerJudge.validateInputs",
-        metadata: {
-          selectedTeam,
-          latestPlayer,
-          timeTypesCount: timeTypes.length,
-        },
-      });
-      return false;
-    }
-
-    return true;
+  const handleTimeTypeClick = (timeTypeId: Id<"time_types">) => {
+    insertTimeLog(timeTypeId, latestPlayer, selectedTeam?.id);
   };
 
   return (
--- src/app/judge-it/ParticipantsJudge.tsx
+++ src/app/judge-it/ParticipantsJudge.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { useMutation } from "convex/react";
-import { api } from "convex/_generated/api";
+import React from "react";
 import { Stack } from "@mui/material";
 import AlertComponent from "../components/AlertComponent";
 import {
   getPlayerName,
   getCurrentHeat,
   getPrevPlayerId,
-  getTimeType,
 } from "@/utils/getUtils";
 import { TIME_TYPE_SAIL } from "@/utils/constants";
-import type { Team, Player, Heat, AlertContext, AlertSeverity } from "@/types";
-import { Id } from "convex/_generated/dataModel";
+import type { Team, Player } from "@/types";
 import useFetchDataConvex from "../hooks/useFetchDataConvex";
 import JudgeButton from "../components/JudgeButton";
+import { useHeatControls } from "../hooks";
 
 interface ParticipantsJudgeProps {
   selectedTeam: Team | null;
@@ -26,135 +23,41 @@ const ParticipantsJudge: React.FC<ParticipantsJudgeProps> = ({
   selectedTeam,
   selectedPlayer,
 }) => {
-  const { alert, heats, players, timeTypes, timeLogs } = useFetchDataConvex();
-  const [alertOpen, setAlertOpen] = useState<boolean>(false);
-  const [alertSeverity, setAlertSeverity] = useState<AlertSeverity>("error");
-  const createTimeLogsBatch = useMutation(api.mutations.createTimeLogsBatch);
-  const [alertText, setAlertText] = useState<string>("");
-  const [alertContext, setAlertContext] = useState<AlertContext | undefined>();
-  const [currentHeat, setCurrentHeat] = useState<Heat | null>(null);
+  const { alert, heats, players, timeLogs } = useFetchDataConvex();
+  const currentHeat = getCurrentHeat(heats, alert);
+
   const playerName = getPlayerName(selectedPlayer?.id || null, players);
+  const prevPlayerId = getPrevPlayerId(
+    selectedTeam?.id || null,
+    currentHeat,
+    timeLogs,
+  );
+  const prevPlayerName = getPlayerName(prevPlayerId, players);
 
-  useEffect(() => {
-    const loadCurrentHeat = async () => {
-      const some = getCurrentHeat(heats, alert);
-      setCurrentHeat(some);
-    };
-    loadCurrentHeat();
-  }, [timeLogs]);
-  const prevPlayerId = currentHeat
-    ? getPrevPlayerId(selectedTeam?.id || null, currentHeat, timeLogs)
-    : '"No previous player"';
-  const prevPlayerName =
-    prevPlayerId && prevPlayerId !== '"No previous player"'
-      ? getPlayerName(prevPlayerId, players)
-      : (prevPlayerId as string);
-  const handleStartStop = async (playerId: string | null) => {
-    if (!playerId || !currentHeat) {
-      setAlertOpen(true);
-      setAlertSeverity("error");
-      setAlertText("Missing player ID or current heat");
-      setAlertContext({
-        operation: "start_stop_timer",
-        location: "ParticipantsJudge.handleStartStop",
-        metadata: {
-          missingField: !playerId ? "playerId" : "currentHeat",
-          selectedTeamId: selectedTeam?.id,
-          selectedPlayerId: selectedPlayer?.id,
-        },
-      });
-      return;
-    }
+  const { handleStartStop } = useHeatControls({}, alert);
 
-    const timeTypeId = getTimeType(TIME_TYPE_SAIL, timeTypes)?.id;
-    if (!timeTypeId) {
-      setAlertOpen(true);
-      setAlertSeverity("error");
-      setAlertText("Could not find sailing time type");
-      setAlertContext({
-        operation: "start_stop_timer",
-        location: "ParticipantsJudge.handleStartStop",
-        metadata: {
-          searchingFor: TIME_TYPE_SAIL,
-          availableTimeTypes: timeTypes.map((t) => t.time_eng),
-        },
-      });
-      return;
-    }
-
-    try {
-      await createTimeLogsBatch({
-        logs: [
-          {
-            team_id: selectedTeam?.id,
-            player_id: prevPlayerId as Id<"players">,
-            time_type_id: timeTypeId,
-            heat_id: currentHeat.id,
-          },
-          {
-            team_id: selectedTeam?.id,
-            player_id: playerId as Id<"players">,
-            time_type_id: timeTypeId,
-            heat_id: currentHeat.id,
-          },
-        ],
-      });
-    } catch (error) {
-      const err = "Error inserting time log: " + (error as Error).message;
-      setAlertOpen(true);
-      setAlertSeverity("error");
-      setAlertText(err);
-      setAlertContext({
-        operation: "start_stop_timer",
-        location: "ParticipantsJudge.handleStartStop",
-        metadata: {
-          step: "insert_time_logs",
-          teamId: selectedTeam?.id,
-          prevPlayerId,
-          currentPlayerId: playerId,
-          heatId: currentHeat.id,
-        },
-      });
-      return;
-    }
-    setAlertOpen(true);
-    setAlertSeverity("success");
-    setAlertText(
-      "Inserted log of type: " +
-        (timeTypes.find((e) => e.id === timeTypeId)?.time_eng || "Unknown"),
-    );
-    setAlertContext({
-      operation: "start_stop_timer",
-      location: "ParticipantsJudge.handleStartStop",
-      metadata: {
-        teamId: selectedTeam?.id,
-        prevPlayerId,
-        currentPlayerId: playerId,
-        heatId: currentHeat.id,
-        timeType: TIME_TYPE_SAIL,
-      },
-    });
+  const handleStartStopClick = (isFullStop: boolean) => () => {
+    if (isFullStop)
+      handleStartStop(prevPlayerId, prevPlayerId, selectedTeam?.id);
+    else handleStartStop(prevPlayerId, selectedPlayer?.id, selectedTeam?.id);
   };
 
   return (
     <>
       <AlertComponent
-        open={alertOpen}
-        severity={alertSeverity}
-        text={alertText}
-        context={alertContext}
-        setOpen={setAlertOpen}
+        open={alert.open}
+        severity={alert.severity}
+        text={alert.text}
+        context={alert.context}
+        setOpen={alert.setOpen}
       />
       <Stack spacing={2} sx={{ width: "100%" }}>
-        <JudgeButton
-          color="error"
-          onClick={() => handleStartStop(prevPlayerId)}
-        >
-          STOP {prevPlayerName} {TIME_TYPE_SAIL}
-        </JudgeButton>
-        <JudgeButton
-          onClick={() => handleStartStop(selectedPlayer?.id || null)}
-        >
+        {prevPlayerId !== null && (
+          <JudgeButton color="error" onClick={handleStartStopClick(true)}>
+            STOP {prevPlayerName} {TIME_TYPE_SAIL}
+          </JudgeButton>
+        )}
+        <JudgeButton onClick={handleStartStopClick(false)}>
           Stop prev and Start {playerName} {TIME_TYPE_SAIL}
         </JudgeButton>
       </Stack>
