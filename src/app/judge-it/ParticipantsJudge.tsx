011e5c0a30a38886721cc2eaad7c940bb1c754a6
Nadja Brix Koch <nadjabrixkoch@proton.me>
2026-05-17 15:24:59 +0200

    refactor: move team select into page components
    Although it might be a bit more code, I feel like this is a better
    division of the components into smaller subcomponents. Then e.g. the
    "MainJudge" component has all components that belongs to the main judge,
    not only the second drop down.
    
    Signed-off-by: Nadja Brix Koch <nadjabrixkoch@proton.me>

--- src/app/judge-it/ParticipantsJudge.tsx
+++ src/app/judge-it/ParticipantsJudge.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Stack } from "@mui/material";
 import AlertComponent from "../components/AlertComponent";
 import {
@@ -9,30 +9,23 @@ import {
   getPrevPlayerId,
 } from "@/utils/getUtils";
 import { TIME_TYPE_SAIL } from "@/utils/constants";
-import type { Team, Player } from "@/types";
 import useFetchDataConvex from "../hooks/useFetchDataConvex";
 import JudgeButton from "../components/JudgeButton";
 import { useCurrentHeat, useHeatControls } from "../hooks";
+import { Id } from "convex/_generated/dataModel";
+import TeamSelect from "../components/TeamSelect";
+import PlayerSelect from "../components/PlayerSelect";
 
-interface ParticipantsJudgeProps {
-  selectedTeam: Team | null;
-  selectedPlayer: Player | null;
-}
-
-const ParticipantsJudge: React.FC<ParticipantsJudgeProps> = ({
-  selectedTeam,
-  selectedPlayer,
-}) => {
+const ParticipantsJudge: React.FC = () => {
   const { alert, heats, players, reload, timeTypes } = useFetchDataConvex();
   const { timeLogs } = useCurrentHeat();
   const currentHeat = getCurrentHeat(heats, alert);
 
-  const playerName = getPlayerName(selectedPlayer?.id || null, players);
-  const prevPlayerId = getPrevPlayerId(
-    selectedTeam?.id || null,
-    currentHeat,
-    timeLogs,
-  );
+  const [teamId, setTeamId] = useState<Id<"teams"> | null>(null);
+  const [playerId, setPlayerId] = useState<Id<"players"> | null>(null);
+
+  const playerName = getPlayerName(playerId, players);
+  const prevPlayerId = getPrevPlayerId(teamId, currentHeat, timeLogs);
   const prevPlayerName = getPlayerName(prevPlayerId, players);
 
   const { handleStartStop } = useHeatControls({}, alert);
@@ -40,14 +33,14 @@ const ParticipantsJudge: React.FC<ParticipantsJudgeProps> = ({
   const sailTypeId = timeTypes.find((tt) => tt.time_eng === TIME_TYPE_SAIL)!.id;
   const teamSailTimeLogs = useMemo(() => {
     return timeLogs.filter(
-      (l) => l.team_id === selectedTeam?.id && l.time_type_id === sailTypeId,
+      (l) => l.team_id === teamId && l.time_type_id === sailTypeId,
     ).length;
   }, [timeLogs]);
 
   const handleStartStopClick = (isFullStop: boolean) => () => {
     if (isFullStop)
-      handleStartStop(prevPlayerId, prevPlayerId, selectedTeam?.id);
-    else handleStartStop(prevPlayerId, selectedPlayer?.id, selectedTeam?.id);
+      handleStartStop(prevPlayerId, prevPlayerId, teamId || undefined);
+    else handleStartStop(prevPlayerId, playerId, teamId || undefined);
     reload();
   };
 
@@ -59,6 +52,12 @@ const ParticipantsJudge: React.FC<ParticipantsJudgeProps> = ({
         text={alert.text}
         context={alert.context}
         setOpen={alert.setOpen}
+      />
+      <TeamSelect selectedTeamId={teamId} setSelectedTeam={setTeamId} />
+      <PlayerSelect
+        selectedTeamId={teamId}
+        selectedPlayer={playerId}
+        setSelectedPlayer={setPlayerId}
       />
       <Stack spacing={2} sx={{ width: "100%" }}>
         {teamSailTimeLogs >= 15 && (
